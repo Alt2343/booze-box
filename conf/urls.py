0bf8610130095553eaaf27d7b8e938adc80b8eda
Alt2343 <stak292001@gmail.com>
2025-08-07 12:03:24 +0500

    base.html

--- conf/urls.py
+++ conf/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('menu.urls', namespace='menu')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+admin.site.site_header = 'Панель администрирования'
+admin.site.index_title = 'Магазин'
